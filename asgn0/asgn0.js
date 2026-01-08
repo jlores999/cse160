ee84776a15476859be3f7a4a6514132c2c91543d
jlores999 <jlores@ucsc.edu>
2026-01-08 11:46:21 -0800

    Update asgn0.js

--- asgn0/asgn0.js
+++ asgn0/asgn0.js
@@ -18,9 +18,10 @@ function main() {
 
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
-  var v = new Vector3([1,2,0]);
-  drawVector(v,"red");
+  //var v = new Vector3([1,2,0]);
+  //drawVector(v,"red");
   //Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
 }
+
