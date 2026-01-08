14ec20a759eafafbf83f77e015d2d6643625d33c
Luis Lores <luis@Luiss-MacBook-Air.local>
2026-01-08 11:12:29 -0800

    Initial commit

---
+++ asgn0/asgn0.js
@@ -0,0 +1,26 @@
+// DrawTriangle.js (c) 2012 matsuda
+//ctx;
+
+/*function drawVector(v, color) {
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  ctx.lineTo(v.elements[0]*20, v.elements[1]*20);
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}*/
+function main() {  
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('example');  
+  if (!canvas) { 
+    console.log('Failed to retrieve the <canvas> element');
+    return false; 
+  } 
+
+  // Get the rendering context for 2DCG
+  var ctx = canvas.getContext('2d');
+  var v = new Vector3([1,2,0]);
+  drawVector(v,"red");
+  //Draw a blue rectangle
+  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
+  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+}
