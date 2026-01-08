75237517b786c472fc834af0201bb80f8f1cfeea
jlores999 <jlores@ucsc.edu>
2026-01-08 12:20:15 -0800

    Update asgn0.js

--- asgn0/asgn0.js
+++ asgn0/asgn0.js
@@ -1,13 +1,13 @@
 // DrawTriangle.js (c) 2012 matsuda
 //ctx;
 
-/*function drawVector(v, color) {
+function drawVector(v, color) {
   ctx.beginPath();
   ctx.moveTo(0,0);
   ctx.lineTo(v.elements[0]*20, v.elements[1]*20);
   ctx.strokeStyle = color;
   ctx.stroke();
-}*/
+}
 function main() {  
   // Retrieve <canvas> element
   var canvas = document.getElementById('example');  
@@ -18,10 +18,11 @@ function main() {
 
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
-  //var v = new Vector3([1,2,0]);
-  //drawVector(v,"red");
+  var v = new Vector3([1,2,0]);
+  drawVector(v,"red");
   //Draw a blue rectangle
   ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
   ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
 }
+
 
